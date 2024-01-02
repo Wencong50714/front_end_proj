3ec5e924aef74c2d5da267185a8bfaa7ee5d969d
dwg <dl20201129@gmail.com>
2024-01-02 18:39:22 +0800

    修改index滚动效果、添加回到顶部按钮

--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var container = document.getElementById('imageContainer');
-  container.classList.add('loaded'); // 添加loaded类以触发过渡效果
-});
-
-document.getElementById('nextButton').addEventListener('click', function() {
-  // 在点击按钮时滚动到下一页
-  document.getElementById('page2').scrollIntoView({ behavior: 'smooth' });
-});
