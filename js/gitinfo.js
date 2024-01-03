83e3185947b141a4b80c9fbbe1a38ea7e26e6685
dwg <dl20201129@gmail.com>
2024-01-03 15:31:56 +0800

    DWG 1.3 adjustment of gitinfo

---
+++ js/gitinfo.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+document.getElementById('githubButton').addEventListener('click', function() {
+    var githubRepoUrl = 'https://github.com/Wencong50714/front_end_proj.git';
+    window.open(githubRepoUrl, '_blank');
+});
+})
